bc9bd3e9ce59222248c37121cf08382fc4007281
ybuchi <yabuch_ch@hotmail.com>
2022-03-28 14:10:22 -0400

    Figured out the input label length problem

--- src/Components/AddItemForm.js
+++ src/Components/AddItemForm.js
@@ -25,34 +25,23 @@ function AddItemForm({newItemForm, newItemFormState, addNewItem}) {
     }
     return (
         <div className="form-container font-ptsans-bold">
-            <h2 className="font-caveat label">Add Items to Your Fridge</h2>
+            <h2 className="font-caveat label">Add Ingredients</h2>
             <form onSubmit={handleNewItemSubmit}>
                 <Grid container direction="row" justify="flex-start" alignItems="flex-start" spacing={1}>
                     <Grid container spacing={1}>
-                        <Grid item sm={4}>
+                        <Grid item sm={10}>
                             {/* Item Name */}
-                                <TextField onChange={handleOnChange} type="text" name="item_name" id="item_name" label="Ingredient Name" value={newItemForm.item_name}></TextField> <br/>
+                                <TextField onChange={handleOnChange} type="text" name="item_name" style={{width: "100%"}} id="item_name" label="Ingredient Name" value={newItemForm.item_name}></TextField> <br/>
                         </Grid>
-                        <Grid item sm={4}>
+                        <Grid item sm={2}>
                             {/* Item Quantity */}
                             <FormGroup >
-                                <TextField onChange={handleOnChange} type="number" name="quantity" id="quantity" value={newItemForm.quantity} label="Quantity"></TextField> <br/>
+                                <TextField onChange={handleOnChange} type="number" name="quantity" style={{width: "50%", margin: "auto"}} id="quantity" value={newItemForm.quantity} label="Quantity"></TextField> <br/>
                             </FormGroup>
                         </Grid>
-                        <Grid item sm={4}>
-                            {/* Item Type */}
-                            <InputLabel id="type">Item Type: </InputLabel>
-                            <select onChange={handleOnChange} name="type" labelid="type" value={newItemForm.type}>
-                                <option value="Vegetable">Vegetable</option>
-                                <option value="Meat">Meat</option>
-                                <option value="Poultry">Poultry</option>
-                            </select> <br />
-                        </Grid>
                     </Grid>
-                </Grid>
-                        
-                            
-                        <Button className="font-ptsans-bold" type="submit" value="Add Item" style={{backgroundColor : "#56BBF1"}}>Add Item</Button>
+                </Grid>  
+                        <Button className="font-ptsans-bold" type="submit" value="Add Item" style={{backgroundColor : "#56BBF1"}} id="btn-add-item">Add Item</Button>
             </form>
         </div>
 
