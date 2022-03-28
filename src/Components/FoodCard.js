0b379bb1b2ff99a2a5a7a6667c5ae649341395d2
ybuchi <yabuch_ch@hotmail.com>
2022-03-27 20:38:59 -0400

    Made some stylistic changes and added cards to the the food items

--- src/Components/FoodCard.js
+++ src/Components/FoodCard.js
@@ -1,4 +1,8 @@
 import React, {useState, useEffect} from "react";
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Col from 'react-bootstrap/Col';
+
 
 function FoodCard({foodItem}) {
 
@@ -8,17 +12,25 @@ function FoodCard({foodItem}) {
         fetch(`https://api.edamam.com/api/food-database/v2/parser?app_id=52ce18e1&app_key=94901fd21fbdbc510e92bd7736f43784&ingr=${foodItem.item_name.trim()}&nutrition-type=cooking`)
         .then(res=> res.json())
         .then(itemAPIData => setFoodItemImage(itemAPIData.hints[0].food.image))
-    },[])
-    
+    },[foodItem.item_name])
+
     return(
-        <>
-            <h2>{foodItem.item_name}</h2>
-            <img src={foodItemImage} alt={foodItem.item_name}/>
-            <h4>{foodItem.type}</h4>
-            <div>
-                <p>{foodItem.quantity}</p>
-            </div>
-        </>
+
+        
+            <Col>
+                <Card>
+                    <Card.Body>
+
+                        <Card.Title><h2>{foodItem.item_name}</h2></Card.Title>
+                        <Card.Img  className="card-image" src={foodItemImage} alt={foodItem.item_name}/>
+                        <Card.Text>
+                            <h4>Type: {foodItem.type}</h4>
+                            <p>Quantity: {foodItem.quantity}</p>
+                        </Card.Text>
+                        
+                    </Card.Body>
+                </Card>
+            </Col>
     )
 }
 
--- src/Components/FoodContainer.js
+++ src/Components/FoodContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import FoodCard from "./FoodCard";
+import Row from 'react-bootstrap/Row';
 
 function FoodContainer({fridgeData, setFridgeData}) {
     const mappedFoodItems = fridgeData.map(foodItem => <FoodCard key={foodItem.id} foodItem={foodItem}/>)
     return(
         <>
-            <h1>Your Fridge</h1>
-            {mappedFoodItems}
+            <h1 className="main-title">Fridge</h1>
+            <Row xs={1} md={2} lg={4}>{mappedFoodItems}</Row>
+            
         </>
 
     )
