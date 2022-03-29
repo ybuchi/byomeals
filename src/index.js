c3a4dc8c1c8802c7256608d7d7ff03a056759799
ybuchi <yabuch_ch@hotmail.com>
2022-03-29 12:24:10 -0400

    Started configuring the routes.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import App from './Components/App'
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from './Components/App';
+import Fridge from './Components/Fridge';
+import Recommended from './Components/Recommended';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<App />}/>
+        <Route path="fridge" element={<Fridge />} />
+        <Route path="/recipes" element={<Recommended />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
