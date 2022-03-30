f5b129a96876a18af6c87f35d69a6a666abfe54a
ybuchi <yabuch_ch@hotmail.com>
2022-03-30 11:43:09 -0400

    Configured basic dashboards

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
 import FridgeDashboard from "./FridgeDashboard";
+import RecipesDashboard from "./RecipesDashboard";
 import "./Dashboard.css"
+import Grid from '@mui/material/Grid';
+
 
 
 function Dashboard() {
@@ -10,18 +13,19 @@ function Dashboard() {
     const foodInFridge = fridgeData.filter((foodObject) => {
         return foodObject.isInFridge && (foodObject.item_name.toLowerCase().trim().includes(searchState.toLowerCase().trim()) || foodObject.type.toLowerCase().trim().includes(searchState.toLowerCase().trim()));
     })
+    const depletedFoods = fridgeData.filter(foodObject=>!foodObject.isInFridge);
 
     return(
         <>
             <h1 className="title">Welcome to BYOMeals</h1>
             <h3 className="subtitle">...where your home cooking dreams come to life</h3>
 
-            <FridgeDashboard fridgeData={ fridgeData } foodInFridge={foodInFridge}/>
+            <Grid container spacing={1} id="item-container">
+                <FridgeDashboard fridgeData={ fridgeData } foodInFridge={foodInFridge} depletedFoods={depletedFoods}/>
+                <RecipesDashboard />
+            </Grid>
 
-            <h2>Recipe Summary</h2>
-            <p><strong>Number of Recipes Cooked: </strong></p>
-            <p><strong>Favorite Recipes: </strong></p>
-            <button>Go to Recipe Finder</button>
+            
         </>
     )
 }
--- src/Components/FridgeDashboard.js
+++ src/Components/FridgeDashboard.js
@@ -1,15 +1,32 @@
 import React from "react";
 import "./FridgeDashboard.css"
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-function FridgeDashboard ({fridgeData, foodInFridge}){
+function FridgeDashboard ({fridgeData, foodInFridge, depletedFoods}){
     return(
-        
+        <Grid item xs={12} sm={12} md={6} lg={6}>
             <Paper elevation={3} className="fridge-summary-paper">
                 <h2 className="fridge-title-summary">Fridge Summary</h2>
-                <p><strong>Number of Items in Fridge: </strong>{foodInFridge.length}</p>
+
+                <Grid container spacing={1}>
+                    <Grid item xs={6}>
+                    <div className="numberOf-itemsInFridge">
+                        <h2 className="number-label">{foodInFridge.length}</h2>
+                        <p><strong>Items in Fridge</strong></p>
+                    </div>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <div className="numberOf-itemsInFridge">
+                        <h2 className="number-label">{depletedFoods.length}</h2>
+                        <p><strong>Depleted Ingredients</strong></p>
+                    </div>
+                    </Grid>
+                </Grid>
+                
                 <button>Go to Fridge</button>
             </Paper>
+        </Grid>
     )
 }
 
